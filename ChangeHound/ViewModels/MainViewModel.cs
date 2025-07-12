5aafa7d3df4855ac984fb9b80963eb7264ffb39b
N3agu <andreei1465@yahoo.com>
2025-07-12 18:48:24 +0000

    Implement filtering logic

--- ChangeHound/ViewModels/MainViewModel.cs
+++ ChangeHound/ViewModels/MainViewModel.cs
@@ -4,19 +4,46 @@ using ChangeHound.Models;
 using ChangeHound.Services;
 using Microsoft.Win32;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace ChangeHound.ViewModels {
     class MainViewModel : ViewModelBase {
         #region Fields
-        public ObservableCollection<FileChange> FileChanges { get; } = new ObservableCollection<FileChange>();
         private readonly FileSystemMonitorService _monitorService;
+        public ObservableCollection<FileChange> FileChanges { get; } = new ObservableCollection<FileChange>();
+        public ICollectionView FilteredFileChanges { get; }
         #endregion
 
         #region Properties
+        // Properties for Filter
+        private string _searchText = string.Empty;
+        public string SearchText {
+            get => _searchText;
+            set {
+                _searchText = value;
+                // notify the ui and reapply the filter
+                OnPropertyChanged();
+                FilteredFileChanges.Refresh();
+            }
+        }
+
+        private string _selectedFilterType;
+        public string SelectedFilterType {
+            get => _selectedFilterType;
+            set {
+                _selectedFilterType = value;
+                OnPropertyChanged();
+                // reapply the filter
+                FilteredFileChanges.Refresh();
+            }
+        }
+
+        // Properties for Export
         public List<string> EventTypes { get; }
         public string SelectedEventType { get; set; }
         public ICommand ExportCommand { get; }
@@ -27,13 +54,32 @@ namespace ChangeHound.ViewModels {
             _monitorService = new FileSystemMonitorService(FileChanges);
             _monitorService.StartMonitoring(@"C:\Test");
 
+            // Initialize Export properties
             EventTypes = new List<string> { "All Events", "Created", "Modified", "Deleted" };
             SelectedEventType = EventTypes.First(); // Default value: "All Events"
             ExportCommand = new DelegateCommand(ExportToFile);
+
+            // Initialize Filter properties
+            _selectedFilterType = EventTypes.First();
+            FilteredFileChanges = CollectionViewSource.GetDefaultView(FileChanges);
+            FilteredFileChanges.Filter = ApplyFilter;
         }
         #endregion
 
         #region Private Methods
+        private bool ApplyFilter(object item) {
+            if (item is not FileChange change) {
+                return false;
+            }
+
+            bool typeMatch = SelectedFilterType == "All Events" || change.EventType == SelectedFilterType;
+
+            bool textMatch = string.IsNullOrWhiteSpace(SearchText) ||
+                             change.FilePath.Contains(SearchText, StringComparison.OrdinalIgnoreCase);
+
+            return typeMatch && textMatch;
+        }
+
         private void ExportToFile(object? parameter) {
             IEnumerable<FileChange> filteredChanges = FileChanges.AsEnumerable();
             if (SelectedEventType != "All Events") {
