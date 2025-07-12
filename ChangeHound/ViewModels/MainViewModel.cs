0697084a2c31796b6dabcd4cce791b8fcf3b46b6
N3agu <andreei1465@yahoo.com>
2025-07-12 18:22:36 +0000

    Delete ExportService, create EventTypes and ExportCommand

--- ChangeHound/ViewModels/MainViewModel.cs
+++ ChangeHound/ViewModels/MainViewModel.cs
@@ -1,15 +1,37 @@
-﻿using ChangeHound.Models;
+﻿using ChangeHound.Commands;
+using ChangeHound.Models;
 using ChangeHound.Services;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ChangeHound.ViewModels {
     class MainViewModel {
+        #region Fields
         public ObservableCollection<FileChange> FileChanges { get; } = new ObservableCollection<FileChange>();
         private readonly FileSystemMonitorService _monitorService;
+        #endregion
 
+        #region Properties
+        public List<string> EventTypes { get; }
+        public string SelectedEventType { get; set; }
+        public ICommand ExportCommand { get; }
+        #endregion
+
+        #region Constructor
         public MainViewModel() {
             _monitorService = new FileSystemMonitorService(FileChanges);
             _monitorService.StartMonitoring(@"C:\Test");
+
+            EventTypes = new List<string> { "All Events", "Created", "Modified", "Deleted" };
+            SelectedEventType = EventTypes.First(); // Default value: "All Events"
+            ExportCommand = new DelegateCommand(ExportToFile);
         }
+        #endregion
+
+        #region Private Methods
+        private void ExportToFile(object? parameter) {
+        
+        }
+        #endregion
     }
 }
