11defd4192532f064a07a6227767ad17331e19ed
N3agu <andreei1465@yahoo.com>
2025-07-12 17:43:07 +0000

    Create FileSystemMonitorService

---
+++ ChangeHound/Services/FileSystemMonitorService.cs
@@ -0,0 +1,17 @@
+﻿using ChangeHound.Models;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace ChangeHound.Services {
+    class FileSystemMonitorService {
+        private readonly FileSystemWatcher _watcher;
+        private readonly ObservableCollection<FileChange> _fileChanges;
+
+        public FileSystemMonitorService(ObservableCollection<FileChange> fileChanges) {
+            _fileChanges = fileChanges;
+            _watcher = new FileSystemWatcher();
+        }
+
+
+    }
+}
