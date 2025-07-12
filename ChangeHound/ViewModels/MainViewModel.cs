8a0367d06885ef26fb5b14794aefd19dcf37b14a
N3agu <andreei1465@yahoo.com>
2025-07-12 17:38:59 +0000

    Create MainViewModel, rename MainWindow to MainView

---
+++ ChangeHound/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+using ChangeHound.Models;
+
+namespace ChangeHound.ViewModels {
+    class MainViewModel {
+        public ObservableCollection<FileChange> FileChanges { get; } = new ObservableCollection<FileChange>();
+    }
+}
