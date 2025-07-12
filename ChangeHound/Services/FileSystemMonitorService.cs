e57c94951875acebec7b129a77722dedf5c270f5
N3agu <andreei1465@yahoo.com>
2025-07-12 17:44:43 +0000

    Implemented logic for file monitoring

--- ChangeHound/Services/FileSystemMonitorService.cs
+++ ChangeHound/Services/FileSystemMonitorService.cs
@@ -12,6 +12,45 @@ namespace ChangeHound.Services {
             _watcher = new FileSystemWatcher();
         }
 
+        public void StartMonitoring(string path) {
+            _watcher.Path = path;
+            _watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            _watcher.IncludeSubdirectories = true;
 
+            _watcher.Changed += OnChanged;
+            _watcher.Created += OnCreated;
+            _watcher.Deleted += OnDeleted;
+            _watcher.Renamed += OnRenamed;
+
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        public void StopMonitoring() {
+            _watcher.EnableRaisingEvents = false;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e) {
+            App.Current.Dispatcher.Invoke(() => {
+                _fileChanges.Add(new FileChange { EventType = "Modified", FilePath = e.FullPath, Timestamp = DateTime.Now });
+            });
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e) {
+            App.Current.Dispatcher.Invoke(() => {
+                _fileChanges.Add(new FileChange { EventType = "Created", FilePath = e.FullPath, Timestamp = DateTime.Now });
+            });
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e) {
+            App.Current.Dispatcher.Invoke(() => {
+                _fileChanges.Add(new FileChange { EventType = "Deleted", FilePath = e.FullPath, Timestamp = DateTime.Now });
+            });
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e) {
+            App.Current.Dispatcher.Invoke(() => {
+                _fileChanges.Add(new FileChange { EventType = "Renamed", FilePath = $"{e.OldFullPath} to {e.FullPath}", Timestamp = DateTime.Now });
+            });
+        }
     }
 }
