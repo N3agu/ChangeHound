e6c6c100796325eebe010c56c698dd7a1c9555de
N3agu <andreei1465@yahoo.com>
2025-07-12 17:51:13 +0000

    Set WindowStartupLocation to CenterScreen and implement temp path for testing

--- ChangeHound/ViewModels/MainViewModel.cs
+++ ChangeHound/ViewModels/MainViewModel.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.ObjectModel;
-using ChangeHound.Models;
+﻿using ChangeHound.Models;
+using ChangeHound.Services;
+using System.Collections.ObjectModel;
 
 namespace ChangeHound.ViewModels {
     class MainViewModel {
         public ObservableCollection<FileChange> FileChanges { get; } = new ObservableCollection<FileChange>();
+        private readonly FileSystemMonitorService _monitorService;
+
+        public MainViewModel() {
+            _monitorService = new FileSystemMonitorService(FileChanges);
+            _monitorService.StartMonitoring(@"C:\Test");
+        }
     }
 }
