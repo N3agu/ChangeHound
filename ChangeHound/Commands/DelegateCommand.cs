6a701f68e080a04564b2ced468b69349ed365962
N3agu <andreei1465@yahoo.com>
2025-07-12 18:08:08 +0000

    Create the DelegateCommand helper

---
+++ ChangeHound/Commands/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace ChangeHound.Commands {
+    public class DelegateCommand : ICommand {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canExecute;
+
+        public DelegateCommand(Action<object?> execute, Predicate<object?>? canExecute = null) {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter) {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter) {
+            _execute(parameter);
+        }
+    }
+}
