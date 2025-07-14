20825906b6591b8526ac6f86335f8e884ad4bd1e
N3agu <andreei1465@yahoo.com>
2025-07-14 13:01:11 +0000

    Create a class to carry the notification payload and a static messaging bus for decoupled communication

---
+++ ChangeHound/Common/EventAggregator.cs
@@ -0,0 +1,22 @@
+﻿namespace ChangeHound.Common {
+    public static class EventAggregator {
+        private static readonly Dictionary<Type, List<object>> _subscribers = new();
+
+        public static void Subscribe<T>(Action<T> action) {
+            var messageType = typeof(T);
+            if (!_subscribers.ContainsKey(messageType)) {
+                _subscribers[messageType] = new List<object>();
+            }
+            _subscribers[messageType].Add(action);
+        }
+
+        public static void Publish<T>(T message) {
+            var messageType = typeof(T);
+            if (_subscribers.ContainsKey(messageType)) {
+                foreach (var subscriber in _subscribers[messageType]) {
+                    (subscriber as Action<T>)?.Invoke(message);
+                }
+            }
+        }
+    }
+}
---
+++ ChangeHound/Common/NotificationMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ChangeHound.Common {
+    public class NotificationMessage {
+        public string Title { get; }
+        public string Message { get; }
+
+        public NotificationMessage(string title, string message) {
+            Title = title;
+            Message = message;
+        }
+    }
+}
